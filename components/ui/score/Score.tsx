9674b7c07ec7058a298101f59dafe5f7df1e827b
Miguel Armas <miguelalbertoaz@gmail.com>
2022-09-24 00:10:24 +0200

    agrega el cambio en los estilos del score

--- components/ui/score/Score.tsx
+++ components/ui/score/Score.tsx
@@ -4,14 +4,12 @@ import css from './Score.module.css'
 
 export const Score = () => {
   const {
-    score: { correct, incorrect, remainingQuestions }
+    score: { remainingQuestions }
   } = useMainContext()
 
   return (
     <ul className={css.Score}>
-      <li>{correct}</li>
       <li>{remainingQuestions}</li>
-      <li>{incorrect}</li>
     </ul>
   )
 }
